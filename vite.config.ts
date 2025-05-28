8fb07189b3ecb5513d5b1887130bb08a2fafc8c7
shoaibashk <shoaibashk.2000@gmail.com>
2025-05-28 20:08:26 +0530

    feat(gh-pages): Added base path url in vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Set the base path for the project in production only
+  base: process.env.NODE_ENV === "production" ? "/landing-page/" : "/",
   plugins: [react()],
-})
+});
