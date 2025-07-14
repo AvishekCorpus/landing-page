422b9e23d0abf7540c0d86ef10df1abf010102d4
shoaibashk <shoaibashk.2000@gmail.com>
2025-07-14 22:27:00 +0530

    Updated BaseUrl for Production

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  // Base path for the application for production builds
-  base: process.env.NODE_ENV === "production" ? "/landing-page/" : "/",
+  // Set the base path for the project in production only
+  // base: process.env.NODE_ENV === "production" ? "/landing-page/" : "/",
+  base: "/",
   plugins: [react()],
 });
